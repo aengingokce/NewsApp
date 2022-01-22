5c498e33ced5c14c1b0cb0576adc9c0f05e985f4
Ahmet Engin Gökçe <aengingokce@gmail.com>
2022-01-23 00:06:53 +0300

    WebService was integrated

---
+++ NewsApp/Services/WebService.swift
@@ -0,0 +1,28 @@
+//
+//  WebService.swift
+//  NewsApp
+//
+//  Created by Ahmet Engin Gökçe on 23.01.2022.
+//
+
+import Foundation
+
+class WebService {
+    
+    struct Resource<T> {
+        let url: URL
+        let parse: (Data) -> T?
+    }
+    
+    func load<T>(resource: Resource<T>, comp: @escaping (T?) -> ()) {
+        URLSession.shared.dataTask(with: resource.url) { data, response, err in
+            if let err = err {
+                debugPrint(err.localizedDescription)
+                comp(nil)
+            }
+            if let data = data {
+                comp(resource.parse(data))
+            }
+        }.resume()
+    }
+}
