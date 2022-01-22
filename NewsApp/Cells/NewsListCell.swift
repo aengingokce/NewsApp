c4508482bbc75bc9809c38f8ea25dee7820098e6
Ahmet Engin Gökçe <aengingokce@gmail.com>
2022-01-22 22:48:26 +0300

    Cell was implemented

---
+++ NewsApp/Cells/NewsListCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsListCell.swift
+//  NewsApp
+//
+//  Created by Ahmet Engin Gökçe on 22.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class NewsListCell: UITableViewCell {
+    
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var headlineImageView: UIImageView!
+}
