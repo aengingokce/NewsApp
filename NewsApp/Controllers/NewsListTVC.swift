ccd2dfb8c62c8217465d4288d6f0653b06bd71da
Ahmet Engin Gökçe <aengingokce@gmail.com>
2022-01-22 23:22:59 +0300

    ui was tried with dummy data but auto layout was failed

--- NewsApp/Controllers/NewsListTVC.swift
+++ NewsApp/Controllers/NewsListTVC.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class NewsListTVC: UITableViewController {
     
+    let news = [
+        ["title": "description"],
+        ["title title": "description title title title title title title title title"],
+        ["title":"description title title title title title title title title title title title title title title title title title title"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +23,21 @@ class NewsListTVC: UITableViewController {
     
     func setUI() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return news.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsListCell") as? NewsListCell else { return UITableViewCell() }
+        cell.headlineLabel.text = news[indexPath.row]["title"]
+        cell.descriptionLabel.text = news[indexPath.row]["description"]
+        return cell
     }
 }
 
