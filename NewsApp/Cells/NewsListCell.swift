a489ea7a7e8d9576ead864890e758542201b0af9
Ahmet Engin Gökçe <aengingokce@gmail.com>
2022-01-25 22:54:42 +0300

    All datas were shown in table view but auto layout still is not good

--- NewsApp/Cells/NewsListCell.swift
+++ NewsApp/Cells/NewsListCell.swift
@@ -13,4 +13,13 @@ class NewsListCell: UITableViewCell {
     @IBOutlet weak var headlineLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var headlineImageView: UIImageView!
+
+}
+
+extension NewsListCell {
+    func configure(vm: ArticleViewModel) {
+        self.headlineLabel.text = vm.title
+        self.descriptionLabel.text = vm.description
+        vm.image { self.headlineImageView.image = $0 }
+    }
 }
--- NewsApp/Controllers/NewsListTVC.swift
+++ NewsApp/Controllers/NewsListTVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewsListTVC: UITableViewController {
     
+    private var categoryListViewModel: CategoryListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,9 +19,26 @@ class NewsListTVC: UITableViewController {
     }
     
     func getArticles() {
-        CategoryService().getAllArticlesforeachCategory { category in
-            print(category)
+        CategoryService().getAllArticlesforeachCategory { [weak self] category in
+            self?.categoryListViewModel = CategoryListViewModel(categories: category)
+            self?.tableView.reloadData()
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.categoryListViewModel == nil ? 0 : self.categoryListViewModel.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.categoryListViewModel.numberOfRowsInSection(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsListCell", for: indexPath) as? NewsListCell else { return UITableViewCell() }
+        let articleVM = self.categoryListViewModel.categoryAtIndex(index: indexPath.section).articleAtIndex(indexPath.row)
+        cell.configure(vm: articleVM)
+        return cell
+        
     }
     
     func setUI() {
